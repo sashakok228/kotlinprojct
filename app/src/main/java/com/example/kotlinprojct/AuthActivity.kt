35388662c44538fff2bdc58da10adc6b47520869
sashakok228 <sasha.antipin6794@mail.ru>
2023-05-31 01:03:20 +0600

    cum

--- app/src/main/java/com/example/kotlinprojct/AuthActivity.kt
+++ app/src/main/java/com/example/kotlinprojct/AuthActivity.kt
@@ -1,11 +1,44 @@
 package com.example.kotlinprojct
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class AuthActivity : AppCompatActivity() {
+    lateinit var dal : Button
+    lateinit var nazad : Button
+    lateinit var nomer : EditText
+    lateinit var password : EditText
+    lateinit var nomerper : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        init()
+        nazad.setOnClickListener{
+            nazad()
+        }
+    }
+    fun init(){
+        dal=findViewById(R.id.dal_auth)
+        nazad=findViewById(R.id.nazad_auth)
+        nomer=findViewById(R.id.nomer_telefona)
+        password=findViewById(R.id.pass_auth)
+    }
+    fun nazad(){
+        val i= Intent(this,MainActivity::class.java)
+        startActivity(i)
+        finish()
+    }
+    fun dal_auth(){
+        var buff : String
+        buff=nomer.text.toString()
+        var nomerper : Int=buff.toInt()
+        buff=nomer.text.toString()
+        var password : Int=buff.toInt()
+        val i= Intent(this,MainActivity::class.java)
+        startActivity(i)
+        finish()
     }
 }
--- app/src/main/java/com/example/kotlinprojct/GameActivity.kt
+++ app/src/main/java/com/example/kotlinprojct/GameActivity.kt
@@ -2,10 +2,33 @@ package com.example.kotlinprojct
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class GameActivity : AppCompatActivity() {
+    lateinit var butyes : Button
+    lateinit var butno : Button
+    lateinit var ivent : TextView
+    lateinit var stat1 : TextView
+    lateinit var stat2 : TextView
+    lateinit var stat3 : TextView
+    lateinit var stat4 : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        init()
+    }
+    fun init(){
+        butyes=findViewById(R.id.yes)
+        butno=findViewById(R.id.no)
+        ivent=findViewById(R.id.text_ivent)
+        stat1=findViewById(R.id.stat_1)
+        stat2=findViewById(R.id.stat_2)
+        stat3=findViewById(R.id.stat_3)
+        stat4=findViewById(R.id.stat_4)
+        stat1.text="50"
+        stat2.text="50"
+        stat3.text="50"
+        stat4.text="50"
     }
 }
